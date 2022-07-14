1c20faf35848d5ae3c96507d3774ed401537d2b0
Alessio Calore <alessio.calore96@gmail.com>
2022-07-14 17:10:44 +0200

    Update package-lock.json, app.component.html, and 30 more files...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
 import { CarouselSectionComponent } from './carousel-section/carousel-section.component';
+import { MinorCarouselOneComponent } from './minor-carousel-one/minor-carousel-one.component';
+import { MinorCarouselTwoComponent } from './minor-carousel-two/minor-carousel-two.component';
+import { TripleIconSectionComponent } from './triple-icon-section/triple-icon-section.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    CarouselSectionComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
-  ],
+  declarations: [AppComponent, HeaderComponent, CarouselSectionComponent, MinorCarouselOneComponent, MinorCarouselTwoComponent, TripleIconSectionComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, NgbCollapseModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/carousel-section/carousel-section.component.ts
+++ src/app/carousel-section/carousel-section.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-carousel-section',
   templateUrl: './carousel-section.component.html',
-  styleUrls: ['./carousel-section.component.scss']
+  styleUrls: ['./carousel-section.component.scss'],
 })
 export class CarouselSectionComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
